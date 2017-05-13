b31e2502b7dcdc261a8ca2ec1f0630dc7625990b
paulopez78 <pau.lopez.78@gmail.com>
2017-05-13 18:17:39 +0200

    cover all use cases with unit tests

--- src/Voting.Domain/VotingAggregate.cs
+++ src/Voting.Domain/VotingAggregate.cs
@@ -10,54 +10,71 @@ namespace Voting.Domain
         public List<IDomainEvent> Events = new List<IDomainEvent>();
         
         private List<string> _topics;
-
         private List<(string topic, int votes)> _votingPair;
         private string _winner;
 
-        public void CreateVoting (string[] topics) => 
+        public void CreateVoting (string[] topics)
+        {
+            topics = topics ?? throw new ArgumentNullException(nameof(topics)); 
             Apply(new VotingCreatedEvent(topics.ToList()));
+        }
 
         public void StartNextVoting()
         {
-            _topics = _topics.Concat(GetWinners(_votingPair)).ToList();
+            if (!string.IsNullOrEmpty(_winner))
+                throw new Exception($"The voting is over, the winner was {_winner}");
 
-            if (_topics.Count == 1) 
+            _topics = _topics.Concat(GetWinners()).ToList();
+
+            if (_topics.Count == 1)
+            {
                 Apply(new VotingFinishedEvent(winner:_topics.Single()));
-            else 
+            }
+            else
+            {
                 Apply(new VotingStartedEvent(
                     _topics.Skip(2).ToList(), 
                     _topics.Take(2).Select(t => (t, 0)).ToList()));
+            }
 
-            IEnumerable<string> GetWinners(List<(string topic, int votes)> votingTopics)
+            IEnumerable<string> GetWinners()
             {
-                if (votingTopics == null) return new List<string>();
-                var maxVotes = votingTopics.Max(t => t.votes);
-                return votingTopics.Where(x => x.votes == maxVotes).Select(x => x.topic);        
+                if (!_votingPair.Any()) return new List<string>();
+                
+                var maxVotes = _votingPair.Max(t => t.votes);
+                return _votingPair.Where(x => x.votes == maxVotes).Select(x => x.topic);        
             }
         }
 
-        public void VoteTopic (string topic) =>
+        public void VoteTopic (string topic)
+        {
+            if (!_votingPair.Any(x => x.topic == topic))
+                throw new Exception($"Selected topic {topic} is not a valid option for voting");
+            
             Apply(new TopicVotedEvent(topic));
+        }
 
         public void Apply(VotingCreatedEvent @event)
         {
             _topics = @event.Topics;
+            _votingPair = new List<(string, int)>();
+            _winner = string.Empty;
             Events.Add(@event);
         }
 
         public void Apply(VotingStartedEvent @event)
         {
             _votingPair = @event.VotingPair;
-            _topics = @event.Topics;            
+            _topics = @event.Topics;
             Events.Add(@event);
         }
 
         public void Apply(TopicVotedEvent @event)
         {
             _votingPair = new List<(string, int)> {
-                _votingPair.Where(x => x.topic == @event.Topic).Select(x => (x.topic, ++x.votes)).First(), 
+                _votingPair.Where(x => x.topic == @event.Topic)
+                    .Select(x => (x.topic, ++x.votes)).First(), 
                 _votingPair.Last()};
-
             Events.Add(@event);
         }
 
--- test/Voting.Domain.Tests/VotingAggregateTest.cs
+++ test/Voting.Domain.Tests/VotingAggregateTest.cs
@@ -9,48 +9,107 @@ namespace Voting.Domain.Tests
     public class VotingAggregateTest
     {
         [Fact]
-        public void CreateVotingTest()
+        public void Given_NoTopics_When_CreateVoting_Then_Exception()
         {
+            // Arrange
             var sut = new VotingAggregate();
-            var topics = new [] {"C#", "F#", "VB.NET", "PowerShell"};
-            sut.CreateVoting(topics);
+            
+            // Act
+            Action result = () => sut.CreateVoting(null);
 
-            var result = sut.Events.First();
-
-            Assert.NotNull(result);
-            Assert.IsType(typeof(VotingCreatedEvent), result);            
-            Assert.Equal(((VotingCreatedEvent)result).Topics.ToArray(), topics);
+            // Assert
+            Assert.ThrowsAny<ArgumentNullException>(result);
         }
 
         [Fact]
-        public void StartNextVotingTest()
+        public void Given_Topics_When_CreateVoting_Then_VotingCreatedEvent()
         {
+            // Arrange
             var sut = new VotingAggregate();
+            var topics = new [] {"C#", "F#", "Java", "JS"};
 
-            sut.CreateVoting(new [] {"C#", "F#", "VB.NET", "PowerShell"});
+            // Act
+            sut.CreateVoting(topics);
+            
+            // Assert
+            var result = sut.Events.OfType<VotingCreatedEvent>().First();
+            Assert.NotNull(result);
+            Assert.Equal(result.Topics, topics);
+        }
+
+        [Fact]
+        public void Given_Topics_When_StartNextVoting_Then_VotingStartedEvent()
+        {
+            // Arrange
+            var sut = new VotingAggregate();
+            var topics = new [] {"C#", "F#", "VB.NET", "PowerShell"};
+
+            // Act
+            sut.CreateVoting(topics);
             sut.StartNextVoting();
 
+            // Assert
             var result = sut.Events.OfType<VotingStartedEvent>().First();
-
             Assert.NotNull(result);
             Assert.Equal(result.VotingPair, new[] {("C#",0),("F#",0)});
             Assert.Equal(result.Topics, new[] {"VB.NET", "PowerShell"});
         }
 
         [Fact]
-        public void VoteTopicTest()
+        public void Given_Topics_When_VoteTopic_Then_VotingFinishedEvent()
         {
+            // Arrange
             var sut = new VotingAggregate();
+            var topics = new [] {"C#", "F#"};
 
-            sut.CreateVoting(new [] {"C#", "F#"});
+            // Act
+            sut.CreateVoting(topics);
             sut.StartNextVoting();
             sut.VoteTopic("C#");
             sut.StartNextVoting();
 
+            // Assert
             var result = sut.Events.OfType<VotingFinishedEvent>().First();
-
             Assert.NotNull(result);
             Assert.Equal(result.Winner, "C#");
+        }
+
+        [Fact]
+        public void Given_2Topics_When_Vote2Topics_Then_VotingStartedEvent()
+        {
+            // Arrange
+            var sut = new VotingAggregate();
+
+            // Act
+            sut.CreateVoting(new [] {"C#", "F#"});
+            sut.StartNextVoting();
+            sut.VoteTopic("C#");
+            sut.VoteTopic("F#");
+            sut.StartNextVoting();
+            
+            // Assert
+            var result = sut.Events.OfType<VotingStartedEvent>().First();
+            Assert.NotNull(result);
+            Assert.Equal(result.VotingPair, new[] {("C#",0),("F#",0)});
+        }
+
+        [Fact]
+        public void Given_FinishedVoting_When_StartVoting_Then_Exception()
+        {
+            // Arrange
+            var sut = new VotingAggregate();
+            var topics = new [] {"C#", "F#"};
+
+            sut.CreateVoting(topics);
+            sut.StartNextVoting();
+            sut.VoteTopic("C#");
+            sut.StartNextVoting();
+
+            // Act
+            Action result = () => sut.StartNextVoting();
+
+            // Assert
+            Assert.ThrowsAny<Exception>(result);
         }
     }
 }
